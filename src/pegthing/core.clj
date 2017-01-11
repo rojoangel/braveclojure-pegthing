f4508cdaec5a2c5c3d85c73e863ba3c0b8ca9019
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-01-11 21:44:44 +0100

    add triangular? to query if a number is triangular

--- src/pegthing/core.clj
+++ src/pegthing/core.clj
@@ -7,7 +7,12 @@
   "Generates lazy sequence of triangular numbers"
   ([] (tri* 0 1))
   ([sum n]
-    (let [new-sum (+ sum n)]
-      (cons new-sum (lazy-seq (tri* new-sum (inc n)))))))
+   (let [new-sum (+ sum n)]
+     (cons new-sum (lazy-seq (tri* new-sum (inc n)))))))
 
 (def tri (tri*))
+
+(defn triangular?
+  "Is the number triangular? e.g. 1, 3, 6, 10, 15, etc"
+  [n]
+  (= n (last (take-while #(>= n %) tri))))

770eab9ead7b7a2b40fa3d505d503704f9a70fdc
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-01-11 21:38:48 +0100

    add tri*: lazy sequence to generate triangular numbers, and tri as its global declaration

--- src/pegthing/core.clj
+++ src/pegthing/core.clj
@@ -1,7 +1,13 @@
 (ns pegthing.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(declare successful-move prompt-move game-over query-rows)
+
+(defn tri*
+  "Generates lazy sequence of triangular numbers"
+  ([] (tri* 0 1))
+  ([sum n]
+    (let [new-sum (+ sum n)]
+      (cons new-sum (lazy-seq (tri* new-sum (inc n)))))))
+
+(def tri (tri*))

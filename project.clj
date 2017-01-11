d0f7ad9ded280fc94281e25efeedabdafaf26f8d
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-01-11 21:26:11 +0100

    add basic lein app structure

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject pegthing "0.1.0-SNAPSHOT"
+  :description "Clojure for the Brave and True - Chapter 5 (Functional Programming) - Peg Thing"
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main ^:skip-aot pegthing.core
+  :profiles {:uberjar {:aot :all}})
